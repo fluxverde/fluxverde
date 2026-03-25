3d949d1f4eb023ee8c09440a944a7ab51c56ca10
Christian Lechner <di.lechner@gmail.com>
2026-03-25 16:01:20 +0100

    adopt mapping

--- BE/src/main/java/io/fluxverde/outgoing/db/company/ManualMeterEntryMapper.java
+++ BE/src/main/java/io/fluxverde/outgoing/db/company/ManualMeterEntryMapper.java
@@ -3,7 +3,7 @@ package io.fluxverde.outgoing.db.company;
 import io.fluxverde.domain.company.meter.ManualMeterEntry;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {MeterMapper.class, CompanyUserMapper.class})
 public interface ManualMeterEntryMapper {
     ManualMeterEntry toDomain(ManualMeterEntryEntity entity);
 
